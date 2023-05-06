6704311a4f13a03bbe344f377f83fc8f04ddf855
TONDERM <96151092+TONDERM@users.noreply.github.com>
2023-05-05 20:48:14 -0400

    CH27 and Maui#2

---
+++ AdventureWorksService/Program.cs
@@ -0,0 +1,30 @@
+using AdventureWorksService.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddDbContext<CustomerContext>(optionsBuilder =>
+optionsBuilder.UseSqlServer(builder.Configuration.GetConnectionString("AdventureWorksDB")));
+// Add services to the container.
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
